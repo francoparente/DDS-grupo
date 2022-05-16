94e2ea792a745730ae6c3fb8043e0f2671671e0b
francoparente <fparente14@gmail.com>
2022-05-16 19:06:38 -0300

    QMP3 Entrega
    Co-authored-by: MaquedaPaul <MaquedaPaul@users.noreply.github.com>
    Co-authored-by: Thomas Candia <ThomasCandia@users.noreply.github.com>
    Co-authored-by: Lucas Blanco <LukBlan@users.noreply.github.com>
    Co-authored-by: Daniel-Aizcorbe <Daniel-Aizcorbe@users.noreply.github.com>

---
+++ qmp3/guardarropa/guardarropa.java
@@ -0,0 +1,22 @@
+class Guardarropa{
+    List<Prenda> prendas;
+
+    constructor(prendas);
+
+    Prenda obtenerPrendaSuperiorAleatoria(){
+        //logica randomizer
+        return prendaSuperiorAleatoria
+    }
+    Prenda obtenerPrendaInferiorAleatoria(){
+        return prendaInferiorAleatoria
+    }
+    Prenda obtenerPrendaCalzadoAleatorio(){
+        return prendaCalzadoAleatoria
+    }
+    Prenda obtenerPrendaAccesorioAleatorio(){
+        return prendaAccesorioAleatorio
+    }
+
+    
+
+}
